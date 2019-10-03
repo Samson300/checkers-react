eee4b276fb1ec96986d1b49cad1a3832d048a334
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-10-02 20:05:05 -0400

    adds css to sytle button in Square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Square () {
     return (
         <div>
-            <button>
+            <button className= "square">
 
             </button>
         </div>
